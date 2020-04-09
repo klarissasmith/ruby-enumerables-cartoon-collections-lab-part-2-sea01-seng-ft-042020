a54c8777a7c09acae7e9c233b6921634e9151b86
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-09 21:31:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,6 +30,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   results = []
   planeteer_calls.find do |element|
-    element == valid_calls.any?
+    element == valid_calls.include?
   end
 end
